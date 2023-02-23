7f5db429e672f621ddfdd36b9b96dfc26bebc19a
gustavo <gm9510@gmailcom>
2023-02-23 16:02:27 -0500

    new filter strategy, using exponential

--- pkg/consumer/util.go
+++ pkg/consumer/util.go
@@ -1,6 +1,7 @@
 package consumer
 
 import (
+	"math"
 	"time"
 
 	"github.com/redis/go-redis/v9"
@@ -8,15 +9,16 @@ import (
 
 func GenerateTimeAndTriesCondition(threshold time.Duration) func(redis.XPendingExt) bool {
 	return func(msg redis.XPendingExt) bool {
-		return (msg.RetryCount * threshold.Milliseconds()) < msg.Idle.Milliseconds()
+		v := float64(threshold.Milliseconds()) + math.Exp(float64(msg.RetryCount))
+		return v < float64(msg.Idle.Milliseconds())
 	}
 }
 
-func Filter(items []redis.XPendingExt, conndition func(item redis.XPendingExt) bool) []redis.XPendingExt {
+func Filter(items []redis.XPendingExt, condition func(item redis.XPendingExt) bool) []redis.XPendingExt {
 	filteredItems := make([]redis.XPendingExt, 0, len(items))
 
 	for _, val := range items {
-		if conndition(val) {
+		if condition(val) {
 			filteredItems = append(filteredItems, val)
 		}
 	}
--- pkg/consumer/util_test.go
+++ pkg/consumer/util_test.go
@@ -1,6 +1,7 @@
 package consumer
 
 import (
+	"math"
 	"testing"
 	"time"
 
@@ -26,16 +27,23 @@ func TestFilters(t *testing.T) {
 		{
 			ID:         "1676389497-0",
 			Consumer:   consumerName,
-			Idle:       3*minIdle + 1*time.Second,
+			Idle:       minIdle + 1*time.Second,
 			RetryCount: 3,
+		},
+		{
+			ID:         "1676389499-0",
+			Consumer:   consumerName,
+			Idle:       minIdle + 1*time.Second + time.Duration(1000000*math.Exp(10)),
+			RetryCount: 10,
 		},
 	}
 
+	t.Log(xPending)
 	result := Filter(xPending, condition)
 
 	t.Log(result)
-	if len(result) != 2 {
-		t.Error("Incomplete result set")
+	if len(result) != 3 {
+		t.Fatal("Incomplete result set")
 	}
 
 	if result[0].ID != xPending[0].ID && result[1].ID != xPending[2].ID {
